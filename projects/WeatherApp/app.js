255091572b33679e367b7c84ec41c592a665a90f
usermane66 <68332445+usermane66@users.noreply.github.com>
2021-04-11 14:20:15 +0100

    adding projects

---
+++ projects/WeatherApp/app.js
@@ -0,0 +1,28 @@
+const button = document.querySelector('.button')
+const inputValue = document.querySelector('.inputValue')
+const name = document.querySelector('.name')
+const desc = document.querySelector('.desc')
+const temp = document.querySelector('.temp')
+const errorMsg = document.getElementById('errorMsg')
+
+button.addEventListener('click', function() {
+	fetch('http://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&units=metric&appid=f68daa637db78a934561270ec0eff1fc')
+	.then(response => response.json())
+	.then(data => {
+		let nameValue = data['name']
+		let descValue = data['weather'][0]['description']
+		let tempValue = data['main']['temp']
+
+		name.innerHTML = nameValue
+		desc.innerHTML = 'The weather right now is ' + descValue + '.'
+		temp.innerHTML = tempValue + ' &#8451'
+		console.log(data)
+	})
+	.catch(err => {
+		errorMsg.innerHTML = 'Please enter a city name.';
+		if (inputValue.value == "") {
+        errorMsg.style.display = "none"
+   		} 
+	  })
+})
+
---
+++ projects/recipe-app/src/App.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState} from "react";
+import Recipe from './Recipe'
+import './App.css';
+
+const App = () => {
+  const APP_ID = '225da60f'; 
+  const APP_KEY = '5c4d9892b07b24a9d8c6b34dccb15408'; 
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('chicken')
+
+  useEffect( () => {
+    getRecipes();
+  }, [query]);
+
+  const getRecipes = async() => {
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const data = await response.json();
+    setRecipes(data.hits)  
+    console.log(data.hits)
+  }
+
+  const updateSearch = e => {
+    setSearch(e.target.value)
+  };
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+  }
+
+  return(
+    <div className="App">
+      <h1>Recipe Finder</h1>
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" placeholder="Type your ingredient here" value={search} onChange={updateSearch} />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+      <div className="recipes">
+        {recipes.map(recipe =>(
+          <Recipe 
+          key={recipe.recipe.label}
+          title={recipe.recipe.label} 
+          calories={recipe.recipe.calories}
+          image = {recipe.recipe.image} 
+          ingredients = {recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
+    </div>
+
+
+  );
+};
+
+export default App;
